cc6d9022f55a8b37aaf8a9a1b7635009afdb9b19
jkurly <69834284+jkurly@users.noreply.github.com>
2020-11-19 18:55:19 -0500

    Update code.py

--- Code/code.py
+++ Code/code.py
@@ -1,9 +1,12 @@
-#step 1
 import time
 
 print("Welcome to the Inventory Counter!")
 time.sleep(1.5)
 print("Please enter your username.")
+
+shirts_stock = {'small':10, 'medium':10, 'large':15, 'xlarge':5}
+pants_stock = {'small':10, 'medium':10, 'large':15, 'xlarge':5}
+hats_stock = {'red':5, 'orange':0, 'yellow':2, 'black':10, 'white':9}
 
 name = input()
 
@@ -12,28 +15,17 @@ username2 = 'Shopper'
 
 
 if name == (username):
-    print('Please wait')
+    print('Please wait...')
     time.sleep(1)
     print('')
-    print('Correct, please enter your password.')
+    print('Correct, please enter your password.')    
     
-if name == (username2):
-    print('Please wait')
-    time.sleep(1)
-    print('')
-    print('Welcome, what are you shopping for today?')
-    print ("Press 1: Check inventory of Shirts.")
-    print ("Press 2: Check inventory of Pants.")
-    print ("Press 3: Check inventory of Hats.")
-    print("Press q: To quit the program.")
-    input ("What would you like to do? ")
-        
 password = input()
 
 password1 = 'Admin'
 
 if password == (password1):
-    print('Please wait')
+    print('Please wait...')
     time.sleep(1)
     print('')
     print('Correct, logging in.')
@@ -49,6 +41,33 @@ if name != (username):
     print('Please wait')
     time.sleep(1)
     print('')
-    print('Incorrect, closing program.')
+    print('Unauthorized personnel, please enjoy looking through our products!')
     time.sleep(1)
     exit
+
+if name == (username2):
+    print('Please wait...')
+    time.sleep(1)
+    print('')
+def menu():
+    print('Welcome, what are you shopping for today?')
+    print ("Press 1: Check inventory of Shirts.")
+    print ("Press 2: Check inventory of Pants.")
+    print ("Press 3: Check inventory of Hats.")
+    print("Press q: To quit the program.")
+    return input ("What would you like to do? ")
+
+run = menu()
+
+while True:
+    if run == "1":
+        s_size = input('What size shirt are you looking for?')
+    def menu():
+        print("Press S: Small")
+        print("Press M: Medium")
+        print("Press L: Large")
+        print("Press XL: Xlarge")
+        run = menu()
+      
+
+
