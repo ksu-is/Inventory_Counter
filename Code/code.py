7abaf12db460bb2eb831af67af328f75c619dcdc
bunderwood21 <69809123+bunderwood21@users.noreply.github.com>
2020-11-19 18:00:44 -0500

    Revert "Update code.py"
    This reverts commit 8da825f952abe75f7efc13216dc564436a728dd3.

--- Code/code.py
+++ Code/code.py
@@ -1,20 +1,2 @@
-#step 1: Greeting
+#step 1
 print("Welcome to the Inventory Counter!")
-#Step 2: Sign-in
-user = input("Username: ")
-pword = input("Password: ")
-user_employee = ["Employee"]
-
-if user in user_employee:
-    password = ["store"]
-    print(pword)
-    if pword in password:
-        print ("Press 1: To add stock. ")
-        print ("Press 2: To check stock. ")
-        print ("Press 3: To remove stock. ")
-        print ("Press q: To quit the program. ")
-        input ("What would you like to do? ")
-else:
-    print("Invalid username/password: ")
-
-
